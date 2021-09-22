09cd170a1345e475842565abffb532168e683797
Justin Chittarath <justinchitt@gmail.com>
2021-09-22 16:24:23 -0600

    commit tree 92b8abf60770cb847d5bd37bf940565a72616e89

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution here
+function findMatching(array, name) {
+    let newArray = array.filter(function(driver){
+         if (name.toUpperCase() === driver.toUpperCase()) {
+             return true;
+        }
+    })
+   return newArray;
+}
+
+function fuzzyMatch(array, string) {
+    let newArray = array.filter(function(letter){
+        if (letter[0] === string[0]) {
+            return true;
+        }
+    })
+    return newArray;
+}
+
+function matchName(array, string) {
+    let newArray = array.filter(function(key){
+        if (string === key.name) {
+            return true;
+        }
+    })
+    return newArray;
+}
